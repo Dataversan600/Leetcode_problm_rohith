2e99fadcab3de3994d5d67fef4126035907b76e0
GAJJALA ROHITH VARSHAN <209209476+Dataversan600@users.noreply.github.com>
2026-09-03 23:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (12.11%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        int max_len = 0;
+        stack<int>st;
+        st.push(-1);
+      
+        for(int i=0 ; i<n ; i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else {
+                    int len = i-st.top();
+                    max_len = max(len,max_len);
+                }
+                   
+            }
+            
+        }
+    
+    return max_len;
+
+    }
+
+};
