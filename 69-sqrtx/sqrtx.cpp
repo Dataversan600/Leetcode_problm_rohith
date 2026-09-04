258d0852d28407c042ced2c08a753ce22eb3282a
GAJJALA ROHITH VARSHAN <209209476+Dataversan600@users.noreply.github.com>
2026-09-04 13:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.31%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l= 0;
+        int r = x;
+        long long ans = 0;
+        while(l<=r){
+            long long mid = l+((r-l)/2);
+            if(mid*mid < x){
+                ans = mid;
+                l= mid+1;
+               
+            }
+            else if(mid*mid>x){
+                r = mid-1;
+            }
+            else return mid;
+        }
+    return ans;
+    
+    }
+};
